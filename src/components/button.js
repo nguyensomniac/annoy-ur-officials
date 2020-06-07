7af8b3fa7b2365015b45b776c2b142723a74fe0f
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2020-06-07 00:28:14 -0700

    Add button component

---
+++ src/components/button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <div style={{
+      display: 'inline-flex',
+      flexDirection: 'row',
+      padding: '8px 16px',
+      background: '#1300FF',
+      color: 'white',
+      borderRadius: '999px',
+      fontWeight: '500',
+      flex: '0 0 auto'
+    }}>
+      Fiona is a qt
+    </div>
+  )
+}
+
+export default Button;
